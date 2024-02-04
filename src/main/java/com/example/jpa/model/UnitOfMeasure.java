82954c4051b02c5224c167fac4fd829482ebfa2a
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-02-04 17:21:55 +0100

    UnitOfMeasure - klasa do ćwiczenia z jpa

---
+++ src/main/java/com/example/jpa/model/UnitOfMeasure.java
@@ -0,0 +1,30 @@
+package com.example.jpa.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class UnitOfMeasure {
+@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+private String description;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
