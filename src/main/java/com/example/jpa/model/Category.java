fda016b3fc1c805a80e3a79b1c30af54daec3013
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-02-04 17:16:18 +0100

    Category - klasa do ćwiczenia z jpa

---
+++ src/main/java/com/example/jpa/model/Category.java
@@ -0,0 +1,40 @@
+package com.example.jpa.model;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    @ManyToMany
+    private Set<Recipe> recipe = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Recipe> getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Set<Recipe> recipe) {
+        this.recipe = recipe;
+    }
+}
